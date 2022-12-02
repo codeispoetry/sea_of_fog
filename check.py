8ae373f9209388ef4772e3841930a99f37293b35
Thomas Rose <thomas.rose@ionos.com>
2022-12-02 10:44:24 +0100

    refactor

--- check.py
+++ check.py
@@ -2,4 +2,4 @@ import kachelmann as satellite
 
 #for day in range(1,30):
  
-satellite.check(satellite.get(2022, 11, 13, 10, 0))
+satellite.check(satellite.get(2021, 11, 7, 7, 0))
--- kachelmann.py
+++ kachelmann.py
@@ -6,14 +6,14 @@ import numpy as np
 
 def get(year=2021, month=11, day=6, hour=10, minute=0):
 
-    url = "https://img3.kachelmannwetter.com/images/data/cache/wwanalyze/download_wwanalyze-de-320-1_%04d_%02d_%02d_262_%02d%02d.png" % (year,month,day,hour, minute)
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36','referer': 'kachelmannwetter.com/de/analyse/wwanalyze-de-320-1.html'}
+    url = "https://img1.kachelmannwetter.com/images/data/cache/wwanalyze/wwanalyze_%04d_%02d_%02d_262_%02d%02d.png" % (year,month,day,hour, minute)
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
     image_path = 'images/' + os.path.basename(url)
 
     if  os.path.exists(image_path) == True:
         print('File exists')
     else:
-        print('download file')
+        print('download file from ' + url)
         response = requests.get(url, headers=headers)
         open(image_path, "wb").write(response.content)
 
@@ -21,16 +21,11 @@ def get(year=2021, month=11, day=6, hour=10, minute=0):
 
 
 def check(image_path):
-    print(image_path)
     img = cv2.imread(image_path)
 
-    cv2.namedWindow("opencv")
-    cv2.imshow("opencv",img)
-    cv2.waitKey(0)
-
-    x = 50
-    y = 0
-    w = 00
+    x = 200
+    y = 170
+    w = 100
     h = 100
 
     a = ( w * h )
@@ -40,10 +35,23 @@ def check(image_path):
     YELLOW_MIN = np.array([40, 235, 235], np.uint8)
     YELLOW_MAX = np.array([60, 255, 255], np.uint8)
 
+    BLUE_MIN = np.array([190, 70, 0], np.uint8)
+    BLUE_MAX = np.array([220, 120, 40], np.uint8)
+
     dst = cv2.inRange(crop_img, YELLOW_MIN, YELLOW_MAX)
     yellow = cv2.countNonZero(dst)
 
+    dst = cv2.inRange(crop_img, BLUE_MIN, BLUE_MAX)
+    blue = cv2.countNonZero(dst)
+
+    if( blue/ a > 0.5 ):
+        print('Percentage of blue is: ' + str(blue/a*100) + '%. Delete.')
+        os.remove(image_path)
+        return
+
     print('Percentage of yellow is: ' + str(yellow/a*100) + '%')
+
+    #show(crop_img)
 
 
 def show(img):
